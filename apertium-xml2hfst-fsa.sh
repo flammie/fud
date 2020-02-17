fa79580a9fb5d41fd1e17457c8d5fc75182a7afb
eevvoor <e@mail>
2020-02-17 12:36:11 +0100

    apertium-xml2hfst-fsa.sh test-file-hardcoded

--- apertium-xml2hfst-fsa.sh
+++ apertium-xml2hfst-fsa.sh
@@ -1,1 +1,1 @@
-grep --color '<e><p><l>[a-z]*<s n="vblex"/></l><r>[a-z]*<s n="vblex"/></r></p></e>' ../../apertium/apertium-fin-deu/*.dix | sed -f apertium-xml2hfst-fsa.sed 
+grep --color '<e><p><l>[a-z]*<s n="vblex"/></l><r>[a-z]*<s n="vblex"/></r></p></e>' ../../apertium/apertium-fin-deu/apertium-fin-deu.fin-deu.dix | sed -f apertium-xml2hfst-fsa.sed 
