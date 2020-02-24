6eff9663253b345402e6a7a594de9395d6666dee
eevvoor <e@mail>
2020-02-24 14:54:13 +0100

    apertium-xml2hfst-fsa.sh-comment-TODO

--- apertium-xml2hfst-fsa.sh
+++ apertium-xml2hfst-fsa.sh
@@ -1,1 +1,8 @@
 grep --color '<e><p><l>[a-z]*<s n="vblex"/></l><r>[a-z]*<s n="vblex"/></r></p></e>' ../../apertium/apertium-fin-deu/apertium-fin-deu.fin-deu.dix | sed -f apertium-xml2hfst-fsa.sed 
+
+# $ echo voittaa | hfst-lookup models/fi_ftb-ud-train.tropical-a1.hfst           
+# hfst-lookup: warning: It is not possible to perform fast lookups with OpenFST, std arc, tropical semiring
+# format automata.
+# Using HFST basic transducer format and performing slow lookups                                           
+# > voittaa       voittaa|VERB|Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin|Voice=Act    10.110037
+# voittaa voittaa|VERB|Case=Lat|InfForm=1|VerbForm=Inf|Voice=Act  10.264188
